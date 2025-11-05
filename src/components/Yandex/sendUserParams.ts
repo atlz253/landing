75d697c3d3a1f062e91f685222b74c1429d5d629
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-11-05 20:35:58 +0300

    исправил отправку параметров

--- src/components/Yandex/sendUserParams.ts
+++ src/components/Yandex/sendUserParams.ts
@@ -1,7 +1,8 @@
+import { METRIKA_COUNTER_ID } from ".";
+
 export function sendUserParams(
-  clientID: string,
   params: Record<string, string | number | boolean>
 ) {
   if (ym === undefined) throw new Error("Яндекс метрика не инициализирована");
-  ym(clientID, "userParams", params);
+  ym(METRIKA_COUNTER_ID, "userParams", params);
 }
